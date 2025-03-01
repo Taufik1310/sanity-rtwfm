1fcf385f7dd982e5ef31f4afd5ca66728e54e2dd
Taufik1310 <taufikupik1310@gmail.com>
2025-03-01 17:47:36 +0700

    change: product data; photoshoot image

--- src/data/products.ts
+++ src/data/products.ts
@@ -12,19 +12,6 @@ const PRODUCTS = [
     ],
     sizes: ["S", "M", "L", "XL"],
   },
-  {
-    id: "sanity-white-t-shirt-drop-2025",
-    name: "SANITY WHITE T-SHIRT DROP [2025]",
-    price: 130000,
-    description:
-      "White 20s cotton short sleeve T-shirts, tubular fit, seamless double needle 2cm collar, taped neck and shoulders, satin & cotton label, double needle sleeve and bottom hem, silver and white plastisol ink screen print.",
-    thumbnail: "sanity-white-tshirt-drop-2025-1.png",
-    pics: [
-      "sanity-white-tshirt-drop-2025-1.png",
-      "sanity-white-tshirt-drop-2025-2.png",
-    ],
-    sizes: ["S", "M", "L", "XL"],
-  },
 ]
 
 export function GetAllProduct() {
