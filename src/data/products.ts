733b19b1fe19b98c29a3a35f0554bdf1f78e986e
Taufik1310 <taufikupik1310@gmail.com>
2025-03-01 17:54:49 +0700

    change: product thumbnail

--- src/data/products.ts
+++ src/data/products.ts
@@ -5,7 +5,7 @@ const PRODUCTS = [
     price: 130000,
     description:
       "Black 20s cotton short sleeve T-shirts, tubular fit, seamless double needle 2cm collar, taped neck and shoulders, satin & cotton label, double needle sleeve and bottom hem, silver and white plastisol ink screen print.",
-    thumbnail: "sanity-black-tshirt-drop-2025-1.png",
+    thumbnail: "sanity-black-tshirt-drop-2025-2.png",
     pics: [
       "sanity-black-tshirt-drop-2025-1.png",
       "sanity-black-tshirt-drop-2025-2.png",
